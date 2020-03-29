5604dc75399c86359f4218bc76928b463c170b84
Code Raivn <riverarenen02@gmail.com>
2020-03-29 14:13:12 +0800

    sidebar and navbar

---
+++ web-template/js/app.js
@@ -0,0 +1,10 @@
+const engageSidebar = () => {
+    const sidebarToggle = document.querySelector('.sidebar-toggle');
+    const sidebar = document.querySelector('.sidebar');
+
+    sidebarToggle.addEventListener('click', () => {
+        sidebar.classList.toggle('open');
+    });
+}
+
+engageSidebar();
