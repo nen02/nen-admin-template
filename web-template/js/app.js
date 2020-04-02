43e58e00c6a6f4a9cede2decb257048aaa902f94
Code Raivn <riverarenen02@gmail.com>
2020-04-02 18:00:56 +0800

    Added dropdown in navbar menu

--- web-template/js/app.js
+++ web-template/js/app.js
@@ -3,7 +3,7 @@ const engageSidebar = () => {
     const sidebar = document.querySelector('.sidebar');
     const navLinks = document.querySelectorAll('.sidebar-nav .nav-items .nav-item');
 
-    sidebarToggle.addEventListener('click', () => {
+    const toggleSidebar = () => {
         sidebar.classList.toggle('open');
         setTimeout(() => {
             [...navLinks].map((link, index) => {
@@ -11,7 +11,29 @@ const engageSidebar = () => {
                 else link.style.animation = `nav-link-fade 500ms ease ${index * 100}ms 1 normal forwards`;
             });
         }, 300);
+    }
+
+    sidebarToggle.addEventListener('click', () => toggleSidebar());
+    sidebarToggle.addEventListener('keydown', event => {
+        if (event.keyCode === 13) toggleSidebar();
     });
 }
 
-engageSidebar();
+const engageNavbar = () => {
+    const navbarDropdown = document.querySelectorAll('.nav-dropdown .dropdown-toggle');
+
+    [...navbarDropdown].map(dropdown => {
+        dropdown.addEventListener('click', function (event) {
+            let dropdownContent = this.parentNode.children[1];
+            dropdownContent.classList.toggle('shown');
+            dropdownContent.classList.toggle('fade-in');
+        });
+    });
+}
+
+const runApp = () => {
+    engageSidebar();
+    engageNavbar();
+}
+
+runApp();
